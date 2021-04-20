91ec372ff7455eb29e41e7189d08e4e621da3a63
osterloh <johnatanosterloh@gmail.com>
2021-04-20 15:11:48 -0300

    Cabeçalho da listagem de produtos

--- src/br/com/senai/controller/ProdutoController.java
+++ src/br/com/senai/controller/ProdutoController.java
@@ -46,6 +46,7 @@ public class ProdutoController {
 	
 	public void consultarProdutos(List<ProdutoModel> produtos) {
 		System.out.println("\n----- PRODUTOS CADASTRASDOS -----\n");
+		System.out.printf("| %10s | %8s | %4s | %9s |\n", "Produto", "Preço", "Quantidade", "R$ Total");
 		for (ProdutoModel produtoModel : produtos) {
 			System.out.println(produtoModel);
 			
