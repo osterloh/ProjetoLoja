211720c93aa010e5b1a2a7700fce2680c7c877b2
osterloh <johnatanosterloh@gmail.com>
2021-05-11 16:52:16 -0300

    Adiciona dados no banco

--- src/br/com/senai/controller/produto/CadastraProduto.java
+++ src/br/com/senai/controller/produto/CadastraProduto.java
@@ -1,13 +1,21 @@
 package br.com.senai.controller.produto;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.Scanner;
 
+import br.com.dao.DataBaseConnection;
 import br.com.senai.model.ProdutoModel;
 
 public class CadastraProduto {
 	
 	Scanner entrada = new Scanner(System.in);
 	ProdutoModel produtoModel;
+	private Connection connection;
+	
+	public CadastraProduto(){
+		connection = DataBaseConnection.getInstance().getConnection();
+	}
 
 	public ProdutoModel cadastrarProduto() {
 		produtoModel = new ProdutoModel();
@@ -20,6 +28,22 @@ public class CadastraProduto {
 		System.out.print("Quantidade:");
 		produtoModel.setQuantidadeDeProduto(entrada.nextInt());
 		produtoModel.setSaldoEmEstoque(produtoModel.getQuantidadeDeProduto() * produtoModel.getPrecoDoProduto());
+		
+		try {
+			String sql = "INSERT INTO produto (nomeDoProduto, precoDoProduto, quantidadeDeProduto, saldoEmEstoque)"
+					+ " VALUES (?, ?, ?, ?)";
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			
+			preparedStatement.setString(1, produtoModel.getNomeDoProduto());
+			preparedStatement.setDouble(2, produtoModel.getPrecoDoProduto());
+			preparedStatement.setInt(3, produtoModel.getQuantidadeDeProduto());
+			preparedStatement.setDouble(4, produtoModel.getSaldoEmEstoque());
+			
+			preparedStatement.execute();
+			
+		} catch (Exception e) {
+			System.out.println("Erro ao cadastrar os dados.");
+		}
 
 		return produtoModel;
 	}
